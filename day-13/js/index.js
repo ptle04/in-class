f0b6b80566667768de140e470656a85650fbcf08
ptle04 <ptle04@uw.edu>
2024-02-12 16:49:18 -0800

    add demo code

--- day-13/js/index.js
+++ day-13/js/index.js
@@ -2,13 +2,35 @@
 'use strict';
 
 // Data: a group of people
-const people = [
-    { name: "Jacob", interest: "Board Games" },
-    { name: "Vishank", interest: "Music" }
+const PEOPLELIST = [
+    { name: "Jacob", interest: "Board Games", location: "Seattle" },
+    { name: "Nathan", interest: "Basketball", location: "Kirkland" },
+    { name: "Thomas", interest: "Baking", location: "Bellevue" },
+    { name: "Ian", interest: "Tennis", location: "Sammamish" },
+  
 ];
 
-// Class for an individual person
+// Functional component for an individual person
+function Person(props) {
+    return (
+        <div class="person">
+            <p>Hello, my name is {props.name} and I am interested in {props.interest}</p>
+            <p>My location is {props.location}!</p>
+        </div>
+    );
+}
 
-// Class to represent a group of people
+// functional component to represent a group of people
+function PeopleGroup(props) {
+    return (
+    (props.group.map((d) => {
+        return <Person key={d.name} name={d.name} interest={d.interest} location={d.location} />
+    }))
+    );
+}
 
 // Render your component in the `main` section
+let myPeople = <PeopleGroup group={PEOPLELIST} />;
+
+//Use ReactDOM.rendere directly
+ReactDOM.render(myPeople, document.querySelector('main'));
