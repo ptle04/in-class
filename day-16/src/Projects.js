0d33ac5167132ad6c7f105e3d89015d5e0e0548b
ptle04 <ptle04@uw.edu>
2024-02-26 17:15:37 -0800

    add code

--- day-16/src/Projects.js
+++ day-16/src/Projects.js
@@ -1,25 +1,24 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import * as d3 from 'd3';
 import { Card, Button, CardBody, CardTitle, CardText, Col } from 'reactstrap';
 
-export class Projects extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            data: []
-        }
-    }
-    componentDidMount() {
-        d3.csv('data/projects.csv', (err, data) => {
-            this.setState({
-                data: data
-            })
-        })
-    }
-    render() {
-        console.log(this.state.data)
-        return <div className="container">Projects
-                 { /* Render a card for each element in your data */ }
-               </div>
-    }
+const Projects = () => {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        d3.csv('data/projects.csv', (err, projectsData) => {
+            if (projectsData) {
+                setData(projectsData);
+            }
+        });
+    }, []);
+
+    return (
+        <div className="container">
+            <h1>Projects</h1>
+            { /* Render a card for each element in your data */ }
+        </div>
+    );
 }
+
+export default Projects;
